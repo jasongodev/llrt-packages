1ee103175d984d3b14dea84da03bf3b4edd7af90
Jason Go <jasongo@jasongo.net>
2025-11-22 04:14:14 +0800

    Add submodule

--- aur/update.sh
+++ aur/update.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-set -e
-
 echo "Updating AUR packages"
 
-for dir in "./"*/ ; do
-    if [ -d "$dir" ]; then
-        pkgdir=$(realpath "$dir")
-        pkgname=$(basename "$pkgdir")
-        echo "==> Updating aur/$pkgname"
-        cd "$pkgdir"
+for dir in ./*/ ; do
+    (
+        if [ -d "$dir" ]; then
+            pkgdir=$(realpath "$dir")
+            pkgname=$(basename "$pkgdir")
+            echo "==> Updating aur/$pkgname"
+            cd "$pkgdir"
 
-        echo "Checking validity of PKGBUILD"
-        namcap PKGBUILD
+            echo "Checking validity of PKGBUILD"
+            namcap PKGBUILD
 
-        echo "Checking for version updates"
-        tmpdir=$(mktemp -d)
-        echo "$tmpdir"
-        cp -r ./ "$tmpdir"
-        cd "$tmpdir"
-        pkgctl version upgrade
-        cp "$tmpdir/PKGBUILD" "$pkgdir/PKGBUILD"
-        rm -rf "$tmpdir"
+            echo "Checking for version updates"
+            tmpdir=$(mktemp -d)
+            echo "$tmpdir"
+            cp -r ./ "$tmpdir"
+            cd "$tmpdir"
+            pkgctl version upgrade
+            cp "$tmpdir/PKGBUILD" "$pkgdir/PKGBUILD"
+            rm -rf "$tmpdir"
 
-        echo "Updating .SRCINFO"
-        cd "$pkgdir"
-        makepkg --printsrcinfo > .SRCINFO
+            echo "Updating .SRCINFO"
+            cd "$pkgdir"
+            makepkg --printsrcinfo > .SRCINFO
 
-        echo "Committing and pushing changes"
-        git add .
-        git commit -m "Version bump"
-        git remote | xargs -L1 git push --all
-    fi
+            echo "Committing and pushing changes"
+            git add .
+            git commit -m "Version bump"
+            git remote | xargs -L1 git push --all
+        fi
+    )
 done
